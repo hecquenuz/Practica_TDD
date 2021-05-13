1f5acd589e38753bace1a92c66a5009c7ed89b22
HectorQuevedo <78912421+hecquenuz@users.noreply.github.com>
2021-05-13 23:18:47 +0100

    Se ha arreglado el código y pasa la prueba

--- TDD/contador.java
+++ TDD/contador.java
@@ -1,23 +1,35 @@
 public class Contador {
 
+    // Propiedades
     private int valor;
     private int incremento;
 
+    // Contructores
     public Contador(int i) {
         this.valor = i;
+        this.incremento = 1;
     }
 
     public Contador() {
         this(0);
     }
 
+    public Contador(int i, int j) {
+        this(i);
+
+        this.incremento = j;
+    }
+
+    // Metodos
     public int getValor() {
 
         return this.valor;
     }
 
     public void incrementa() {
-        this.valor++;
+
+        this.valor = this.valor + this.incremento;
 
     }
+
 }
